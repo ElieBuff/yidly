b5172a1bf71026bb4aecb2d93fd2669f2d93d199
Yehonathan Sharvit <viebel@viebel-VirtualBox.(none)>
2013-03-22 07:07:33 +0200

    1st draft

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dashboard::Application.config.secret_token = 'e1f1881b5d7142f3ab4465baa0315b3959a7104691ae9eca9a54e6a17c30d97b19c97d0f8fbc462ef241762c64b2fe5d1d7ebe65e6a2e0ba860106c46c739f4e'
