e3b592acc45463749ab68ae86661645558d3df28
Yehonathan Sharvit <viebel@gmail.com>
2013-03-26 23:29:01 +0200

    improve seed

--- app/models/record.rb
+++ app/models/record.rb
@@ -2,4 +2,7 @@ class Record < ActiveRecord::Base
   attr_accessible :email, :name, :project_id, :status
   validates_presence_of :name, :project_id
   belongs_to :project
+  def summary
+     "name: #{self.name}, project: #{self.project.name}, user: #{self.project.user.email}, status: #{self.status}"
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,14 @@
 # See http://railsapps.github.com/rails-environment-variables.html
 puts 'CREATE DEFAULT USERS'
 3.times {|user_id| 
-  user = User.find_or_create_by_email! :id=>user_id, :name => "test_#{user_id}", :email => "test_#{user_id}@yidly.com", :password => "99999999", :password_confirmation => "99999999"
+  user = User.find_or_create_by_email! :name => "test_#{user_id}", :email => "test_#{user_id}@yidly.com", :password => "99999999", :password_confirmation => "99999999"
   rand(5).times {|prj_id|
-    prj = Project.create! :name => "yidly_prj_#{prj_id}_user_#{user_id}", :user_id=>user_id
-    rand(4).times {|i|
-      record = Record.create! :name => "Candidate_#{i}_prj_#{prj_id}_user_#{user_id}", :project_id=> prj.id, :status => ["received", "reviewed", "contacted", "meeting_scheduled", "passed_to_manager", "offer", "closed"].sample
-      p record
+    prj = Project.find_or_create_by_name! :name => "prj_#{rand(5)}", :user_id=>user.id
+    rand(4).times {
+      Record.create! :name => "Candidate_#{rand(100)}", :project_id=> prj.id, :status => ["received", "reviewed", "contacted", "meeting_scheduled", "passed_to_manager", "offer", "closed"].sample
     }
   }
+}
+Record.all.each {|record|
+  puts record.summary
 }
