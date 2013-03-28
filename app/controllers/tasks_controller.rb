58a2e72d6de1d9f2c2e0e83f7e53e5e916798c46
Yehonathan Sharvit <viebel@gmail.com>
2013-03-28 22:37:43 +0200

    improve tasks.html ui

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,14 @@
 class TasksController < ApplicationController
   def index
-    @tasks = current_user.records.map {|record|
+    tasks = current_user.records.map {|record|
       record.to_task
     }
+    #tasks = (1..rand(20)).map {
+    #  {:action => rand(300), :name => rand(100)}
+    #
     respond_to do |format|
       format.html
-      format.json { render json: @tasks}
+      format.json { render json: tasks}
     end
   end
 end
