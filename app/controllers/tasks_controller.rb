a606f2c30a35a4411bb4f97cf82bec673afd7df9
Yehonathan Sharvit <viebel@gmail.com>
2013-03-28 06:09:48 +0200

    implement tasks.html with d3 and ajax

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
   def index
-    tasks = current_user.records.map {|record|
+    @tasks = current_user.records.map {|record|
       record.to_task
     }
     respond_to do |format|
-      format.json { render json: tasks}
+      format.html
+      format.json { render json: @tasks}
     end
   end
 end
