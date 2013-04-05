6db04f6d81b35f49bf99fa2cd594c64418c05979
Yehonathan Sharvit <viebel@gmail.com>
2013-04-05 16:45:47 +0300

    crossdomain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
-  after_filter :set_access_control_headers
+  before_filter :set_access_control_headers
 
   def set_access_control_headers 
-    headers['Access-Control-Allow-Origin'] = '*' 
-    headers['Access-Control-Request-Method'] = '*' 
+    headers['Access-Control-Allow-Origin'] = '*.herokuapp.com' 
+    headers['Access-Control-Request-Method'] = '*.herokuapp.com' 
   end
   protect_from_forgery
 end
