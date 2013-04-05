5687d6ee41e5b525e3948293de5ef819d1bb3481
Yehonathan Sharvit <viebel@gmail.com>
2013-04-05 17:08:27 +0300

    cross domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   before_filter :set_access_control_headers
 
   def set_access_control_headers 
-    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Origin'] = 'http://yidly-dashboard-local.herokuapp.com'
     headers['Access-Control-Request-Method'] = '*' 
   end
-  #protect_from_forgery
+  protect_from_forgery
 end
