946bb1a459f306e002b6512991fd4568c47fc8aa
Yehonathan Sharvit <viebel@gmail.com>
2013-04-07 09:50:19 +0300

    spec records_controller

--- spec/controllers/records_controller_spec.rb
+++ spec/controllers/records_controller_spec.rb
@@ -8,17 +8,28 @@ describe RecordsController do
   end
 
   describe "GET 'show'" do
-    
-    it "should be successful" do
+    before :each do
       get :show, :id => @record.id, :format => :json
+    end
+    it "should be successful" do
       response.should be_success
     end
-    
+
     it "should find the right record" do
-      get :show, :id => @record.id, :format => :json
       assigns(:record).should == @record
     end
-    
   end
 
+  describe "GET 'reschedule_in_sec'" do
+    before :each do
+      get :reschedule_in_sec, :id => @record.id, :delay => "1000", :format => :json
+      @response_json = JSON.parse(response.body)
+    end
+    it "should postpone the actionable_at attribute" do
+      Time.parse(@response_json['actionable_at']).should be_within(1).of(Time.at(Time.now.utc + 1000))
+    end
+    it "should postpone the actionable_at attribute" do
+      Time.parse(@response_json['actionable_at']).should be_within(1).of(Time.at(Time.now.utc + 1000))
+    end
+  end
 end
