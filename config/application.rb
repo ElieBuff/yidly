f0285e3f4ad590c3daf7b70b450e70e70a59de03
Elie Buff <e@yidly.com>
2013-04-16 12:21:07 -0400

    dust

--- config/application.rb
+++ config/application.rb
@@ -57,7 +57,7 @@ module Dashboard
     config.assets.enabled = true
 
     # Heroku requires this to be false
-    config.assets.initialize_on_precompile=true
+    config.assets.initialize_on_precompile=false
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     # don't generate RSpec tests for views and helpers
