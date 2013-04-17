88bea8dd9c0d24b587eba5d945203d72a7ee9eb6
Elie Buff <e@yidly.com>
2013-04-17 16:39:22 -0400

    ttt

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,11 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require dust-core
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
 //= require d3
-//= require dust-core
-//= require_tree .
 //= require_tree ./templates
 
  	
--- config/application.rb
+++ config/application.rb
@@ -54,7 +54,7 @@ module Dashboard
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = false
+    config.assets.enabled = true
 
     # Heroku requires this to be false
     config.assets.initialize_on_precompile=false
