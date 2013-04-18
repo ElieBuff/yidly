c19c3c6fcec9573867d8fc4f304ecf82ca294630
Elie Buff <e@yidly.com>
2013-04-18 18:45:26 -0400

    test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,5 +10,12 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
+//= require d3
+//= require dust
+//= require_tree .
+
 
  	
