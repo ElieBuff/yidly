a62e39deaf18503c8687a87c85dfee96df1398bf
Yehonathan Sharvit <viebel@gmail.com>
2013-04-18 23:57:32 +0300

    cleanup

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,7 +1,8 @@
-
-var taskID;
-
 $(function() {
+    var taskID;
+    function getProcessedTask(data) {
+        return UTILS.renderDust("record_small", UTILS.formatTimeStampInDict(data, 'actionable_at'));
+    }
     function reloadData(){
         $.getJSON('tasks.json', function(data) {
             function filterActionableBefore(tasks, datePoint) {
@@ -17,14 +18,11 @@ $(function() {
                 divs.enter().append('div').attr('class', 'task-container').html(createHtml);
                 divs.exit().remove();
             }
-
-
             displayTasks(d3.select('.today_tasks'), filterActionableBefore(data, new Date()));
             displayTasks(d3.select('.all_tasks'), data);
         });
     }
     reloadData();
-
     $( "#Reschedule" ).dialog({ 
         position: { my: "center", at: "center", of: ".reschedule_box" },
         autoOpen: false,  
@@ -33,32 +31,22 @@ $(function() {
         width:150,
         modal: true
     });
-
-
     $(document).on("click", "body", function(){
         taskID = '';
         $('#Reschedule').dialog('close');
     });
-
     $(document).on("dragstart", ".task", function(e){
         var id = e.target.getAttribute('id')
         e.originalEvent.dataTransfer.setData("text/plain", id); 
     });
-
-
-
     $(document).on("dragover", ".drop_box", function(e){
         e.preventDefault();
     });
-
-
-
     $('.drop_done,.drop_cancel').on('drop', function(e) {
         function getTask(id) {
             var title = $('#'+ id + ' .title').text();
             return '<div id="' + id + '" class="drop_data">' + title + '</div>'
         }
-
         e.preventDefault();
         var id = e.originalEvent.dataTransfer.getData("text/plain");
         $(this).append(getTask(id));
@@ -69,29 +57,21 @@ $(function() {
         });
         reloadData();
     });
-
     $('.drop_reschedule').on('drop', function(e) {
         e.preventDefault();
         taskID = e.originalEvent.dataTransfer.getData("text/plain");
         $('#Reschedule').dialog('open');
     });
-
-
-
     $('.date_radio').click(function() {
         var val = $(this).val();
-        if(val == 'today')
-    {
-        $('.today_tasks').show();
-        $('.all_tasks').hide();
-    }
-        else
-    {
-        $('.today_tasks').hide();
-        $('.all_tasks').show();
-    }
+        if(val == 'today') {
+            $('.today_tasks').show();
+            $('.all_tasks').hide();
+        } else {
+            $('.today_tasks').hide();
+            $('.all_tasks').show();
+        }
     });
-
     $('.reschedule_time').click(function() {
         var jQueryThis = $(this);
         function url(){
@@ -106,11 +86,9 @@ $(function() {
         in_a_week: 7*24*HOUR
                 }[futureStr];
             }
-
             function recordId() {
                 return taskID.replace('task', '');
             }
-            console.log( '/records/' + recordId() + '/reschedule_in_sec.json?delay=' + timeInSec(futureStr()));
             return '/records/' + recordId() + '/reschedule_in_sec.json?delay=' + timeInSec(futureStr());
         }
         $.getJSON(url(), function(data) {
@@ -120,10 +98,3 @@ $(function() {
         reloadData();
     });
 });
-
-function getProcessedTask(data)
-{
-    return UTILS.renderDust("record_small", UTILS.formatTimeStampInDict(data, 'actionable_at'));
-}
-
-
