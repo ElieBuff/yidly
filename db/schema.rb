4288953cfd7b720b45e0f8ab5157e2a5b035f2d2
Elie Buff <e@yidly.com>
2013-04-23 00:40:12 -0400

    ss

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130421134208) do
-
-  create_table "columns", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-    t.integer  "position"
-  end
+ActiveRecord::Schema.define(:version => 20130415080814) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
@@ -37,7 +30,6 @@ ActiveRecord::Schema.define(:version => 20130421134208) do
     t.integer  "trial_count"
     t.integer  "reschedule_count"
     t.string   "status"
-    t.datetime "rejected_at"
   end
 
   create_table "stages", :force => true do |t|
