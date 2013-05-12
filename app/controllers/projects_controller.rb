bd48aacb983b96938b23373ea1cd84ac69a650e1
Yehonathan Sharvit <viebel@gmail.com>
2013-05-12 10:36:45 +0300

    update image actions and project page css

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -52,14 +52,19 @@ class ProjectsController < UserAuthenticatedController
     @project = Project.new params[:project]
     @project.user = current_user
     @project.save
+=begin
+    inflating: decision.jpg
+      inflating: panel interview.jpg
+          inflating: hiring manager.jpg
+            inflating: phone interview.jpg
+=end
     [
-      ["received", "review", "http://www.tienganh123.com/file/luyen-thi-toeic/part3%20practice/test%204.jpg"], 
-      ["reviewed", "contact", "http://www.skinz.org/free-clipart/email-graphics/envelope-circle-clipart.gif"], 
-      ["contacted", "schedule a meeting", "http://www.rock.k12.nc.us//cms/lib6/NC01000985/Centricity/Domain/153/Phone.jpg"], 
-      ["meeting_scheduled", "pass to manager", "http://www.best-of-web.com/_images_300/Realistic_Style_Quarterback_Throwing_the_Football_100308-164108-094042.jpg"], 
-      ["passed to manager",""], 
-      ["offer",""], 
-      ["closed",""]
+      ["Review", "review", "/assets/actions/review.jpg"], 
+      ["Initial Contact", "contact", "/assets/actions/email.jpg"], 
+      ["Phone Screen", "schedule a meeting", "/assets/actions/phone_interview.jpg"], 
+      ["First Interview", "pass to manager", "/assets/actions/hiring_manager.jpg"], 
+      ["Further Interviews","wait", "/assets/actions/panel_interview.jpg"], 
+      ["Decision","wait", "/assets/actions/decision.jpg"], 
     ].each {|stage, action, icon|
       Stage.create! :name => stage, :action => action, :icon => icon, :project_id => @project.id
     }
